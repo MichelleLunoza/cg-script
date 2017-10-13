f57970b5a8741dd1d2ccc741312b47c49b4e658c
Chaelgutierrez <michael@vielsoft.com>
2017-10-13 08:04:43 +0800

    UPDATE: Fetch Server IP Address

--- proxy.js
+++ proxy.js
@@ -1,12 +1,16 @@
+/*
+    This script is for corsing the server for external API
+    Note: change host to you server host
+*/
 var cors_proxy = require('cors-anywhere');
-var host = process.env.HOST || '0.0.0.0';
+var ip = require('ip');
+var host = ip.address();
 var port = process.env.PORT || 8080;
-
 
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
-    console.log('Successfully running cors on' + host + ':' + port);
+    console.log('Successfully running cors on ' + host + ':' + port);
 });
